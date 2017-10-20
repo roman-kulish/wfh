ae54af5770495006fa609a02cd6569fcc64e128c
Roman Kulish <roman.kulish@gmail.com>
2017-10-20 22:22:47 +1100

    Initial commit

---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"bytes"
+	"encoding/json"
+
+	"crypto/sha1"
+	"fmt"
+	"math/rand"
+
+	"strconv"
+
+	"github.com/roman-kulish/wfh/slash"
+)
+
+const (
+	endpoint    = "/wfh"
+	bucket      = "https://storage.googleapis.com/wfh/%x.jpg"
+	msgToday    = "@here %s is working from today"
+	msgTomorrow = "@here %s will work from home tomorrow"
+	msgMonday   = "@here %s will work from home on Monday"
+)
+
+type SlashCommandHandler struct {
+	Handler func(cmd slash.CommandRequest) (slash.CommandResponse, error)
+	http.HandlerFunc
+}
+
+func (sc SlashCommandHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var jsonData bytes.Buffer
+
+	if r.Method != http.MethodPost {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	req, err := slash.NewCommandRequest(r)
+
+	if err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	res, err := sc.Handler(req)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	if req.Command != endpoint {
+		http.NotFound(w, r)
+		return
+	}
+
+	if err := json.NewEncoder(&jsonData).Encode(&res); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData.Bytes())
+}
+
+func main() {
+	handler := SlashCommandHandler{
+		Handler: func(req slash.CommandRequest) (slash.CommandResponse, error) {
+			var message = msgToday
+
+			loc, _ := time.LoadLocation("Australia/Sydney")
+			now := time.Now().In(loc)
+			then := time.Date(now.Year(), now.Month(), now.Day(), 10, 15, 0, 0, loc)
+
+			if now.After(then) {
+				if now.Weekday() > 4 {
+					message = msgMonday
+				} else {
+					message = msgTomorrow
+				}
+			}
+
+			message = fmt.Sprintf(message, req.UserName)
+			index := rand.Intn(250)
+
+			if index == 0 {
+				index++
+			}
+
+			hash := sha1.Sum([]byte(strconv.Itoa(index)))
+
+			res := slash.NewInChannelCommandResponse(message)
+
+			res.Attachments = append(res.Attachments, slash.Attachment{
+				ImageUrl: fmt.Sprintf(bucket, hash),
+			})
+
+			return res, nil
+		},
+	}
+
+	mux := *http.NewServeMux()
+	mux.Handle(endpoint, handler)
+
+	server := http.Server{
+		Addr:         ":8080",
+		Handler:      &mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 3,
+		IdleTimeout:  time.Second * 10,
+	}
+
+	server.ListenAndServe()
+}
---
+++ slash/slash.go
@@ -0,0 +1,92 @@
+package slash
+
+import (
+	"net/http"
+)
+
+const (
+	inChannel = "in_channel"
+	ephemeral = "ephemeral"
+)
+
+type CommandRequest struct {
+	Token          string
+	TeamId         string
+	TeamDomain     string
+	EnterpriseId   string
+	EnterpriseName string
+	ChannelId      string
+	ChannelName    string
+	UserId         string
+	UserName       string
+	Command        string
+	Text           string
+	ResponseUrl    string
+	TriggerId      string
+}
+
+type CommandResponse struct {
+	ResponseType string       `json:"response_type,omitempty"`
+	Text         string       `json:"text"`
+	Attachments  []Attachment `json:"attachments,omitempty"`
+}
+
+type Attachment struct {
+	Fallback   string  `json:"fallback,omitempty"`
+	Color      string  `json:"color,omitempty"`
+	Pretext    string  `json:"pretext,omitempty"`
+	AuthorName string  `json:"author_name,omitempty"`
+	AuthorLink string  `json:"author_link,omitempty"`
+	AuthorIcon string  `json:"author_icon,omitempty"`
+	Title      string  `json:"title,omitempty"`
+	TitleLink  string  `json:"title_link,omitempty"`
+	Text       string  `json:"text,omitempty"`
+	Fields     []Field `json:"fields,omitempty"`
+	ImageUrl   string  `json:"image_url,omitempty"`
+	ThumbUrl   string  `json:"thumb_url,omitempty"`
+	Footer     string  `json:"footer,omitempty"`
+	FooterIcon string  `json:"footer_icon,omitempty"`
+	Ts         int     `json:"ts,omitempty"`
+}
+
+type Field struct {
+	Title string `json:"title"`
+	Value string `json:"value"`
+	Short string `json:"short"`
+}
+
+func NewCommandResponse(text string) CommandResponse {
+	return CommandResponse{
+		ResponseType: ephemeral,
+		Text:         text,
+	}
+}
+
+func NewInChannelCommandResponse(text string) CommandResponse {
+	return CommandResponse{
+		ResponseType: inChannel,
+		Text:         text,
+	}
+}
+
+func NewCommandRequest(r *http.Request) (CommandRequest, error) {
+	if err := r.ParseForm(); err != nil {
+		return CommandRequest{}, err
+	}
+
+	return CommandRequest{
+		Token:          r.PostForm.Get("token"),
+		TeamId:         r.PostForm.Get("team_id"),
+		TeamDomain:     r.PostForm.Get("team_domain"),
+		EnterpriseId:   r.PostForm.Get("enterprise_id"),
+		EnterpriseName: r.PostForm.Get("enterprise_name"),
+		ChannelId:      r.PostForm.Get("channel_id"),
+		ChannelName:    r.PostForm.Get("channel_name"),
+		UserId:         r.PostForm.Get("user_id"),
+		UserName:       r.PostForm.Get("user_name"),
+		Command:        r.PostForm.Get("command"),
+		Text:           r.PostForm.Get("text"),
+		ResponseUrl:    r.PostForm.Get("response_url"),
+		TriggerId:      r.PostForm.Get("trigger_id"),
+	}, nil
+}
