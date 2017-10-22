59c661e8e6ab9c2e69079dfecfbed1ac6dcf8b4b
Roman Kulish <roman.kulish@gmail.com>
2017-10-22 13:30:17 +1100

    Package restructure

--- main.go
+++ main.go
@@ -4,19 +4,113 @@ import (
 	"net/http"
 	"time"
 
+	"bytes"
+	"crypto/sha1"
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"os"
+	"strconv"
 
-	"github.com/roman-kulish/wfh/generator"
+	"github.com/roman-kulish/wfh/slash"
 )
 
-const port = 80
+const (
+	command     = "/wfh"
+	bucket      = "https://storage.googleapis.com/wfh/%x.jpg"
+	msgToday    = "<!here> <@%s> is working from home today"
+	msgTomorrow = "<!here> <@%s> will be working from home tomorrow"
+	msgMonday   = "<!here> <@%s> will be working from home on Monday"
+	imgTitle    = "My excuse is ..."
+	port        = 80
+)
 
 var addr string
 
+type slashCommandHandler struct {
+	Handler func(cmd slash.CommandRequest) (slash.CommandResponse, error)
+	http.HandlerFunc
+}
+
+func (sc slashCommandHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var jsonData bytes.Buffer
+
+	if r.Method != http.MethodPost {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+
+	req, err := slash.NewCommandRequest(r)
+
+	if err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	res, err := sc.Handler(req)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	if req.Command != command {
+		http.NotFound(w, r)
+		return
+	}
+
+	encoder := json.NewEncoder(&jsonData)
+	encoder.SetEscapeHTML(false)
+
+	if err := encoder.Encode(&res); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData.Bytes())
+}
+
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	mux := *http.NewServeMux()
-	mux.Handle(generator.Command, generator.NewHandler())
+
+	mux.Handle(command, slashCommandHandler{
+		Handler: func(req slash.CommandRequest) (slash.CommandResponse, error) {
+			var message = msgToday
+
+			loc, _ := time.LoadLocation("Australia/Sydney")
+			now := time.Now().In(loc)
+			then := time.Date(now.Year(), now.Month(), now.Day(), 10, 15, 0, 0, loc)
+
+			if now.Weekday() > 4 {
+				message = msgMonday
+			} else if now.After(then) {
+				message = msgTomorrow
+			}
+
+			message = fmt.Sprintf(message, req.UserId)
+			index := rand.Intn(250)
+
+			if index == 0 {
+				index++
+			}
+
+			hash := sha1.Sum([]byte(strconv.Itoa(index)))
+
+			res := slash.NewInChannelCommandResponse(message)
+
+			res.AddAttachment(slash.Attachment{
+				Title:    imgTitle,
+				ImageUrl: fmt.Sprintf(bucket, hash),
+			})
+
+			return res, nil
+		},
+	})
 
 	server := http.Server{
 		Addr:         addr,
